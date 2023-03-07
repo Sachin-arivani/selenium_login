fc564a17a2f128e1dbf00d03e64c6c7fa28446aa
SACHIN ARIVANI <sachin@arivani.net>
2023-03-07 12:30:38 +0530

    selenium

---
+++ loginseleniumeg2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("loginseleniumeg2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("loginseleniumeg2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b29500d4-68e6-4efd-a99d-a9ca2dd4a76f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ loginseleniumeg2/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace loginseleniumeg2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://geotab1.arivani.com/login/");
+            driver.FindElement(By.Name("username")).SendKeys("incharlottenow@gmail.com");
+            driver.FindElement(By.Name("password")).SendKeys("Test@123");
+            driver.FindElement(By.Id("save_btn")).Click();
+
+            //driver.Quit();
+        }
+    }
+}
